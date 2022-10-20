992e918342e201817984f8a11b9c73db41feb7ae
Alt ° <bachurinadnepro@gmail.com>
2022-10-20 10:11:05 +0300

    First commit

--- calculatorProject.java
+++ calculatorProject.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class calculatorProject {
+import java.util.Scanner;
+
+public class calculatorProject {
+    public void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Скорость автомобиля: ");
+        double speed = in.nextDouble();
+        System.out.println("Выберите состояние дороги\n1. Сухая\n2. Мокрая");
+    }
+    public void stopping_distance(double speed){
+        double distance = speed;
+        System.out.println("Тормозной путь: " + distance);
+    }
 }
