007a66c945332acfea2b22a91694e968d560b931
Alt ° <bachurinadnepro@gmail.com>
2022-10-20 10:19:37 +0300

    Add some points

--- calculatorProject.java
+++ calculatorProject.java
@@ -6,7 +6,19 @@ public class calculatorProject {
 
         System.out.println("Скорость автомобиля: ");
         double speed = in.nextDouble();
-        System.out.println("Выберите состояние дороги\n1. Сухая\n2. Мокрая");
+        System.out.println("Выберите тип дороги\n1. Сухой асфальт\n2. Мокрая дорога\n3. Укатанный снег\n4. Обледенелая дорога");
+        int roadType = in.nextInt();
+        double roadTypeNum = 0;
+        switch (roadType){
+            case 1: roadTypeNum = 0.8;
+                    break;
+            case 2: roadTypeNum = 0.55;
+                break;
+            case 3: roadTypeNum = 0.25;
+                break;
+            case 4: roadTypeNum = 0.25;
+                break;
+        }
     }
     public void stopping_distance(double speed){
         double distance = speed;
